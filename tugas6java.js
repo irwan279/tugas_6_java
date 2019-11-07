39036cd3d23dbb51a97e7c187de8e56864c11dca
Irwan M Faizal <irwan279217@gmail.com>
2019-11-07 08:52:35 +0700

    tugas 6 java

---
+++ tugas6java.js
@@ -0,0 +1,11 @@
+function perulangan() {
+  for (var i = 0; i < 20; i++) {
+    if (i%2) {
+      console.log(i);
+    }
+    else {
+      console.log("angka genap : ",i);
+    }
+  }
+}
+perulangan()
